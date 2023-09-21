b4ad68df8224c712e65ff45ea2c2cfc30f12d86b
codegenius2 <lucifer231220@gmail.com>
2023-09-22 02:56:55 +0800

    release 1.0.0
    fix: possible bug that locks mouse as down

--- src/home/panel/panel.tsx
+++ src/home/panel/panel.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import {useSnapshot} from "valtio/react";
 import {appState, PanelSelection} from "../../state/app-state.ts";
-import {controlState} from "../../state/control-state.ts";
 import {ChatList} from "./chat-list/chat-list.tsx";
 import {cx, escapeSpaceKey} from "../../util/util.tsx";
 import {Global} from "./global/global.tsx";
@@ -12,16 +11,17 @@ import {layoutState} from "../../state/layout-state.ts";
 export const Panel: React.FC = () => {
 
     const {currentChatId, panelSelection} = useSnapshot(appState)
+    const [isMouseDown, setIsMouseDown] = useState(false)
 
     const onMouseUpOrDown = useCallback((p: PanelSelection) => {
         appState.panelSelection = p
     }, [])
 
     const onMouseEnter = useCallback((p: PanelSelection) => {
-        if (controlState.isMouseLeftDown) {
+        if (isMouseDown) {
             appState.panelSelection = p
         }
-    }, [])
+    }, [isMouseDown])
 
     const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
         layoutState.settingPanelScrollOffset = e.currentTarget.scrollTop
@@ -30,6 +30,10 @@ export const Panel: React.FC = () => {
     return (
         <div className="flex h-full select-none flex-col gap-3 w-full"
              onKeyDown={escapeSpaceKey}
+             onMouseDown={() => setIsMouseDown(true)}
+             onMouseUp={() => setIsMouseDown(false)}
+             onMouseLeave={() => setIsMouseDown(false)}
+             onBlur={() => setIsMouseDown(false)}
         >
             <div className="flex items-center rounded-xl font-medium min-h-12  gap-y-2
             p-1 bg-white bg-opacity-40">
@@ -37,7 +41,10 @@ export const Panel: React.FC = () => {
                     className={cx("flex w-1/3 justify-center items-center h-full rounded-lg transition-all duration-150",
                         panelSelection === "chats" ? "bg-white/[0.8]" : "hover:bg-white/[0.4]")}
                     onMouseUp={() => onMouseUpOrDown("chats")}
-                    onMouseDown={() => onMouseUpOrDown("chats")}
+                    onMouseDown={(e) => {
+                        e.stopPropagation()
+                        onMouseUpOrDown("chats")
+                    }}
                     onMouseEnter={() => onMouseEnter("chats")}
                 >
                     <p className="text-center">Chats</p>
@@ -102,5 +109,4 @@ export const Panel: React.FC = () => {
     )
 }
 
-
-const ninjia = <div className="min-h-[14rem] text-transparent select-none pointer-events-none">ninja</div>
+const ninjia = <div className="min-h-[14rem] text-transparent select-none pointer-events-none" data-pseudo-content="ninja"></div>
